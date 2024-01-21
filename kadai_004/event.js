97dd93d3749d9deea173c0c1bcfccdf623de94df
sakura-0329 <147309375+sakura-0329@users.noreply.github.com>
2024-01-21 19:48:04 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,13 @@
+$(window).on('load', (e) => {
+    // 処理
+$('div').text('loadイベントが発生しました');
+  });
+  
+
+$(window).on('scroll', (e) => {
+    // 処理
+$('div').text('scrollイベントが発生しました');
+          });
+  
+
+  
